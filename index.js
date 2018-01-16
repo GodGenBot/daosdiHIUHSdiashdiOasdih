f8c177550c0179a03e471dbc977c9a4d28e5cb7f
GodGenBot <35499077+GodGenBot@users.noreply.github.com>
2018-01-16 17:57:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const PREFIX = "!";
 
-const TOKEN = "";
+const TOKEN = "NDAyODY4NzkwNTM2MTc1NjE2.DT_AVQ.3fokMBbghLn_iDtzVG7-pu0wVjk";
 
 var bot = new Discord.Client();
 
